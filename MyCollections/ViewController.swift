4e17903938e0fd2cf46b2d4832a7433a5317acd1
Aleksandr Aniskin <aleksandr.nskn@yandex.ru>
2021-01-13 10:06:38 +0300

    Initial Commit

---
+++ MyCollections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCollections
+//
+//  Created by Aleksandr Aniskin on 13.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
